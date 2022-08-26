07f47129a55dfea008b40ff54e687d87a01a794b
Natascha Manger <rommy3132@gmail.com>
2022-08-26 23:49:18 +0200

    Solutions day 13 and day 14.

---
+++ 2020/day13/Program.cs
@@ -0,0 +1,129 @@
+﻿namespace day13
+{
+    internal class Program
+    {
+        static List<string> ReadInputData()
+        {
+            string fileName = "puzzleInput.txt";
+            string? filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName);
+
+            List<string> rawData = new();
+            using (var myReader = new StreamReader(filePath))
+            {
+                // First line is your arrival time
+                string nextLine = myReader.ReadLine();
+                rawData.Add(nextLine);
+
+                // Second line is ','-separated list of bus numbers
+                nextLine = myReader.ReadLine();
+                string[] busTimes = nextLine.Split(',');
+                foreach (string busTime in busTimes) rawData.Add(busTime);
+            }
+            return rawData;
+        }
+
+        static long inv(long a, long m)
+        {
+            long m0 = m, t, q;
+            long x0 = 0, x1 = 1;
+
+            if (m == 1)
+                return 0;
+
+            // Apply extended Euclid Algorithm
+            // calculate gcd and a*x+b*y = gcd(a,b) (y is not calculated)
+            while (a > 1)
+            {
+                // q is quotient
+                q = a / m;
+
+                t = m;
+                // m is remainder now, process same as euclid's algorithm
+                m = a % m; a = t;
+
+                t = x0;
+
+                x0 = x1 - q * x0;
+
+                x1 = t;
+            }
+
+            // Make x1 positive
+            if (x1 < 0)
+                x1 += m0;
+
+            return x1;
+        }
+
+        static long FindMinTimestamp(int[] num, int[] rem, int k)
+        {
+            // Compute product
+            // of all numbers
+            long prod = 1;
+            for (int i = 0; i < k; i++)
+                prod *= num[i];
+
+            // Initialize result
+            long result = 0;
+
+            // Apply above formula
+            for (int i = 0; i < k; i++)
+            {
+                long pp = prod / num[i];
+                result += rem[i] * inv(pp, num[i]) * pp;
+            }
+
+            return result % prod;
+        }
+
+        static void Main(string[] args)
+        {
+            List<string> rawData = ReadInputData();
+            
+            int myArrivalTime = int.Parse(rawData[0]);
+
+            int earliestBus = 0;
+            int timeToEarliestBus = myArrivalTime;
+            for(int i = 1; i < rawData.Count; i++)
+            {
+                // entries x are ignored for now
+                if (rawData[i] == "x") continue;
+
+                int busNumber = int.Parse(rawData[i]);
+                int nextBusArrival = (myArrivalTime/busNumber +1)*busNumber;
+                int timeToNextBus = nextBusArrival-myArrivalTime;
+                if(timeToNextBus < timeToEarliestBus)
+                {
+                    earliestBus = busNumber;
+                    timeToEarliestBus = timeToNextBus;
+                }
+            }
+            Console.WriteLine("ID of earliest bus: {0}, Wait time: {1}, ID x Wait time: {2}",
+                earliestBus,timeToEarliestBus,earliestBus*timeToEarliestBus);
+
+            //prepare data for part 2: List all bus IDs and corresponding arrival delays vs time t
+            List<int> busIDs = new List<int>();
+            List<int> delayVsFirstBus = new List<int>();
+            int t;
+            for(int i = 1; i < rawData.Count; i++)
+            {
+                // entries x are ignored
+                if (rawData[i] == "x") continue;
+
+                t = int.Parse(rawData[i]);
+                busIDs.Add(t);
+                delayVsFirstBus.Add(t-(i-1));
+            }
+            long minTimestamp = FindMinTimestamp(busIDs.ToArray(), delayVsFirstBus.ToArray(), busIDs.Count);
+
+            for(int i=0; i<busIDs.Count; i++)
+            {
+                Console.WriteLine(busIDs[i] + " " + delayVsFirstBus[i]);
+                 
+            }
+
+            Console.WriteLine("Fist timestamp at which busses arrive with specified delays: {0}", minTimestamp);
+
+        }
+    }
+}
---
+++ 2020/day14/Program.cs
@@ -0,0 +1,227 @@
+﻿namespace day14
+{
+    internal class Program
+    {
+        static long BitArrayToLong(byte[] bitArray)
+        {
+            long intNum = 0;
+            for (int i = 0; i < bitArray.Length; i++)
+            {
+                //Console.Write(bitArray[i]);
+                if (bitArray[i] == 0) continue;
+                intNum += (long)Math.Pow(2, 35 - i);
+            }
+            //Console.Write(" "+ intNum + "\n");
+            return intNum;
+        }
+
+        static byte[] LongToBitArray(long number)
+        {
+            //Console.Write(number + " ");
+            List<byte> byteStack = new List<byte>();
+            long remainder;
+            while (number > 0)
+            {
+                number = Math.DivRem(number, 2, out remainder);
+                byteStack.Add((byte) remainder);
+            }
+            int length = byteStack.Count;
+            byte[] result = new byte[36];
+            for(int i = 0; i < 36; i++)
+            {
+                if(i < length)
+                {
+                    result[35-i] = byteStack[i];
+                }
+                else
+                {
+                    result[35 - i] = 0;
+                }
+                   
+            }
+            /*
+            for(int i = 0; i< result.Length; i++ )
+            {
+                Console.Write(result[i]);
+            }
+            Console.WriteLine();
+            */
+            return result;
+        }
+
+        static Dictionary<int, long> DecoderVersion1(string filePath)
+        {
+            Dictionary<int, long> memory = new();
+            using (var myReader = new StreamReader(filePath))
+            {
+                string? nextLine;
+                char[] currentMask = new char[36];
+                while (myReader.Peek() != -1)
+                {
+                    nextLine = myReader.ReadLine();
+                    string[] splitString = nextLine.Split("=", StringSplitOptions.TrimEntries);
+                    if (splitString[0] == "mask")
+                    {
+                        currentMask = splitString[1].ToCharArray();
+                    }
+                    else
+                    {
+                        string[] splitAgain = splitString[0].Split(new char[] { '[', ']' },
+                            StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                        long input = long.Parse(splitString[1]);
+                        byte[] inputBitArray = LongToBitArray(input);
+                        for (int i = 0; i < currentMask.Length; i++)
+                        {
+                            if (currentMask[i] == 'x' || currentMask[i] == 'X') continue;
+                            inputBitArray[i] = (byte)int.Parse(currentMask[i].ToString());
+                        }
+                        int inputKey = int.Parse(splitAgain[1]);
+                        if (memory.ContainsKey(inputKey))
+                        {
+                            memory[inputKey] = BitArrayToLong(inputBitArray);
+                        }
+                        else
+                        {
+                            memory.Add(inputKey, BitArrayToLong(inputBitArray));
+                        }
+
+                    }
+
+                }
+
+            }
+            return memory;
+        }
+
+        static Dictionary<long, long> DecoderVersion2(string filePath)
+        {
+            Dictionary<long, long> memory = new();
+            using (var myReader = new StreamReader(filePath))
+            {
+                string? nextLine;
+                char[] currentMask = new char[36];
+                while (myReader.Peek() != -1)
+                {
+                    nextLine = myReader.ReadLine();
+                    string[] splitString = nextLine.Split("=", StringSplitOptions.TrimEntries);
+                    if (splitString[0] == "mask")
+                    {
+                        currentMask = splitString[1].ToCharArray();
+                    }
+                    else
+                    {
+                        string[] splitAgain = splitString[0].Split(new char[] { '[', ']' },
+                            StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                        long address = long.Parse(splitAgain[1]);
+                        byte[] addressBitArray = LongToBitArray(address);
+
+                        //apply mask (2 represents floating bit)
+                        List<int> floatingBitPositions = new List<int>();
+                        for (int i = 0; i < currentMask.Length; i++)
+                        {
+                            if (currentMask[i] == '1' )
+                            {
+                                addressBitArray[i] = 1;
+                            }
+                            else if (currentMask[i] == 'x' || currentMask[i] == 'X')
+                            {
+                                addressBitArray[i] = 2;
+                                floatingBitPositions.Add(i);
+                            }
+                        }
+                        List<byte[]> addressList = new List<byte[]> { addressBitArray};
+                        for(int i = 0; i < floatingBitPositions.Count; i++)
+                        {
+                            List<byte[]> copyList = new List<byte[]> {};
+                            for(int j=0; j<addressList.Count; j++)
+                            {
+                                //copy all values in array in addressList[j] and add new array to copy list
+                                byte[] tmp = new byte[addressList[j].Length];
+                                addressList[j].CopyTo(tmp, 0);
+                                copyList.Add(tmp);
+
+                                //set floating bit to 0 in original array and 1 in copy
+                                addressList[j][floatingBitPositions[i]] = 0;
+                                copyList[j][floatingBitPositions[i]] = 1;
+
+                            }
+                            //merge both lists
+                            addressList.AddRange(copyList);
+                            
+                        }
+
+                        long input = long.Parse(splitString[1]);
+                        foreach (byte[] addr in addressList)
+                        {   
+                            long outAddress = BitArrayToLong(addr);
+
+                            if (memory.ContainsKey(outAddress))
+                            {
+                                memory[outAddress] = input;
+                            }
+                            else
+                            {
+                                memory.Add(outAddress, input);
+                            }
+                        }
+                    }
+
+                }
+
+            }
+            return memory;
+        }
+
+        static void Main(string[] args)
+        {
+            string fileName = "puzzleInputTest.txt";
+            string? filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName);
+
+            Dictionary<int, long> memory = DecoderVersion1(filePath);
+            long sumValues = 0;
+            foreach (long value in memory.Values)
+            {
+                sumValues += value;
+            }
+            Console.WriteLine("Sum of all values in memory (Test 1): {0}",sumValues);
+
+            fileName = "puzzleInput.txt";
+            filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName);
+            
+            memory = DecoderVersion1(filePath);
+            sumValues = 0;
+            foreach (long value in memory.Values)
+            {
+                sumValues += value;
+            }
+            Console.WriteLine("Sum of all values in memory (V 1): {0}", sumValues);
+
+            fileName = "puzzleInputTestPart2.txt";
+            filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName);
+
+            Dictionary<long,long> memoryV2 = DecoderVersion2(filePath);
+            sumValues = 0;
+            foreach (KeyValuePair<long,long> pair in memoryV2)
+            {
+                //Console.WriteLine(pair.Key);
+                sumValues += pair.Value;
+            }
+            Console.WriteLine("Sum of all values in memory (Test 2): {0}", sumValues);
+
+            fileName = "puzzleInput.txt";
+            filePath = Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, fileName);
+
+            memoryV2 = DecoderVersion2(filePath);
+            sumValues = 0;
+            foreach (KeyValuePair<long, long> pair in memoryV2)
+            {
+                //Console.WriteLine(pair.Key);
+                sumValues += pair.Value;
+            }
+            Console.WriteLine("Sum of all values in memory (V 2): {0}", sumValues);
+
+        }
+    }
+}
