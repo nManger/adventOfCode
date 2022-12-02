b66bb991759cbb3189f758a92df2fc5edcba3558
Natascha Manger <rommy3132@gmail.com>
2022-12-02 21:59:55 +0100

    Solution to day 1, modified .gitignore for VS

---
+++ day1/day1.h
@@ -0,0 +1,12 @@
+﻿// day1.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
+#include <fstream>
+#include <string>
+#include <vector>
+#include <tuple>
