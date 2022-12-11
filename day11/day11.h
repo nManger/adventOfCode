3354f4ddc012c558150c1c0e7f27eba8933c86d7
Natascha Manger <rommy3132@gmail.com>
2022-12-11 21:12:38 +0100

    solution day 11

---
+++ day11/day11.h
@@ -0,0 +1,50 @@
+﻿// day11.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <vector>
+#include <tuple>
+#include <queue>
+#include <string>
+
+#include <regex>
+#include <cstdlib>
+#include <functional>
+
+static long LCM_ALL_MONKEYS;
+
+template<typename T>
+class Monkey
+{
+	std::queue<T> itemsHeld;
+	std::function<T(T)> operation;
+	T testDivisor=1;
+	int monkeyTrue=0;
+	int monkeyFalse=0;
+	T numItemsInspected =0;
+
+public:
+	
+	Monkey(const std::vector<T>& items,std::function<T(T)> operation, T testDivisor, int monkeyTrue, int monkeyFalse);
+	Monkey(const Monkey&);
+	bool hasItems();
+	std::tuple<int, T> inspectNextItem();
+	void receiveItem(T item);
+	T getNumItemsInspected();
+};
+
+
+template<typename T>
+class MonkeyGame
+{
+	std::vector<Monkey<T>> monkeyList;
+	
+
+public:
+	MonkeyGame(std::string filename);
+	T playGame(int turns);
+};
