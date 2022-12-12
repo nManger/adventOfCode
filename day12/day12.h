175e911c661a88ad88c9d0bfbec8dde48bb44bc7
Natascha Manger <rommy3132@gmail.com>
2022-12-12 22:28:14 +0100

    Solution day 12. Dijkstra ftw.

---
+++ day12/day12.h
@@ -0,0 +1,16 @@
+﻿// day12.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include<fstream>
+
+#include<vector>
+#include<string>
+#include<set>
+#include<functional>
+
+#include<limits>
+
+// TODO: Reference additional headers your program requires here.
