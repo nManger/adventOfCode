d9f034f0d20915ac264b0294316b5f6007ee0e41
Natascha Manger <rommy3132@gmail.com>
2022-12-15 21:23:29 +0100

    Solution day 15 (using semi-? brute force)

---
+++ day15/day15.h
@@ -0,0 +1,23 @@
+﻿// day15.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <vector>
+#include <string>
+#include <tuple>
+#include <set>
+
+#include <cstdlib>
+#include <regex>
+
+
+struct Sensor{
+	std::tuple<int,int> position;
+	std::tuple<int,int> nearestBeacon;
+	
+	int distanceSensorBeacon;
+};
