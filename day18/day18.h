3717c22180f89099ac829359e7bce98a6d61c2d7
Natascha Manger <rommy3132@gmail.com>
2022-12-18 22:39:07 +0100

    Solution day 18.

---
+++ day18/day18.h
@@ -0,0 +1,16 @@
+﻿// day18.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+
+#include <vector>
+#include <string>
+#include <tuple>
+
+#include <regex>
+#include <limits>
+#include <cstdlib>
+
