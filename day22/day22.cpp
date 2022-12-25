b6d5fb2ee0870a5b0e38c9d841523031f854d4b9
Natascha Manger <rommy3132@gmail.com>
2022-12-26 00:10:49 +0100

    added cube for part 2 and hard-coded transformations

--- day22/day22.cpp
+++ day22/day22.cpp
@@ -3,7 +3,6 @@
 
 #include "day22.h"
 
-enum Cardinal{ right,down,left,up, CARDINAL_SIZE};
 
 int modulo(int a, int b)
 {
@@ -15,6 +14,7 @@ std::tuple<std::vector<std::string>, std::vector<std::variant<int,char>>> readPu
 {
 	std::vector<std::string> map;
 	std::vector<std::variant<int, char>> directions;
+	int deltaX = 0; 
 
 	std::string line;
 	std::ifstream myFile(filename);
@@ -26,6 +26,8 @@ std::tuple<std::vector<std::string>, std::vector<std::variant<int,char>>> readPu
 		while (!line.empty() && !myFile.eof())
 		{
 			map.push_back(line);
+			//keep track of largest line size for padding later
+			if (line.size() > deltaX) deltaX = line.size();
 
 			getline(myFile, line);
 		}
@@ -56,30 +58,239 @@ std::tuple<std::vector<std::string>, std::vector<std::variant<int,char>>> readPu
 		std::cout << "Could not open file " + filename + ".\n";
 		exit(1);
 	}
+	
+	//pad rows to max map witdh
+	for (std::string& row : map)
+	{
+		row.append(std::string(deltaX - row.size(), ' '));
+	}
 
 	return { map,directions };
 }
 
-long long walkMap(const std::vector<std::string>& inMap, const std::vector<std::variant<int, char>>& directions)
+std::vector<std::string> getCubeFace(const std::vector<std::string> &map, int x, int y, int delta)
+{
+	std::vector<std::string> face;
+
+	for (int i = y; i < y + delta; ++i)
+	{
+		face.push_back(map[i].substr(x, delta));
+	}
+
+	return face;
+}
+
+//cube shape 0 is test, 1 is puzzle cube
+Cube createCubeFromMap(std::vector<std::string> map, int cubeShape)
+{
+	Cube puzzleCube;
+	puzzleCube.map = map;
+
+	//get tile size
+	int delta = std::gcd(map.size(), map.at(0).size());
+	int x, y;
+	Side face;
+
+
+	if (cubeShape == 0)
+	{
+		//Top
+		face = Top;
+		x = 2 * delta; y = 0;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = {x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { left,down,down,down };
+
+		//Back
+		face = Back;
+		x = 0; y = delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { right,up,up,down };
+
+		//Left
+		face = Left;
+		x = delta; y = delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { right,right,left,right };
+
+		//Front
+		x = 2 * delta; y = delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { down,down,left,up };
+
+		//Down
+		x = 2 * delta ; y = 2*delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { right,up,up,up };
+
+		//Right
+		x = 3 * delta ; y = 2 * delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = right;
+		puzzleCube.edgeDirectionChanges.at(face) = { left,right,left,left };
+
+	}
+	else if (cubeShape == 1)
+	{
+		//Top
+		face = Top;
+		x = delta; y = 0;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { down,down,right,right };
+
+		//Left
+		face = Left;
+		x = 0; y = 2*delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = left;
+		puzzleCube.edgeDirectionChanges.at(face) = { left,left,left,up };
+
+		//Front
+		face = Front;
+		x = delta; y = delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { up,down,down,up };
+
+		//Bottom
+		face = Bottom;
+		x = delta; y = 2* delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = up;
+		puzzleCube.edgeDirectionChanges.at(face) = { left,left,left,up };
+
+		//left
+		face = Left;
+		x = 0; y = 2 * delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = left;
+		puzzleCube.edgeDirectionChanges.at(face) = { right,down,right,right };
+
+		//Right
+		x = 0 ; y = 3 * delta;
+		puzzleCube.cubeFaceCoordinateOffset.at(face) = { x,y };
+		puzzleCube.cubeFaces.at(face) = getCubeFace(map, x, y, delta);
+		puzzleCube.faceUpSideDirection.at(face) = left;
+		puzzleCube.edgeDirectionChanges.at(face) = {up ,down,down,up };
+
+	}
+
+	return puzzleCube;
+}
+
+/* Create cube independent from specific net (unfinished)
+Cube createCubeFromMap(std::vector<std::string> map)
+{
+	Cube puzzleCube;
+	size_t x = 0, y = 0; //top-left corner coords
+	Side side, prevSide;
+	std::set<Side> checkedSides;
+
+	//get side tile size
+	int delta = std::gcd(map.size(), map.at(0).size());
+	
+	
+	std::queue<std::tuple<Side,Side,int,int>> sidesToCheck; //new side, prev side, x, y
+	//1st side = top side
+
+	side = Top;
+	x = map.at(0).find_first_not_of(' ');
+	sidesToCheck.push({ side,side,x,y });
+
+	while (!sidesToCheck.empty())
+	{
+		auto [side, prevSide, x, y] = sidesToCheck.front();
+		sidesToCheck.pop();
+		
+		//process current side
+		puzzleCube.cubeFaceCoordinateOffset.at(side) = { x,y };
+		puzzleCube.cubeFaces.at(side) = getCubeFace(map, x, y, delta);
+
+		//find relation of prev to current tile
+		int topside;
+		if (side == prevSide) topside = up; //1st side (Top side) exception
+		//side is right of prev
+		else if (x > get<0>(puzzleCube.cubeFaceCoordinateOffset[prevSide]))
+		{
+			if (prevSide == Top) 
+			{
+				puzzleCube.
+			}
+			else if(prevSide == Bottom)
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+		//side is left of prev
+		else if (x < get<0>(puzzleCube.cubeFaceCoordinateOffset[prevSide]))
+		{
+
+		}
+		//side is below prev
+		else
+		{
+
+		}
+
+		puzzleCube.faceUpSideDirection.at(side) = puzzleCube.faceUpSideDirection.at(prevSide);
+		checkedSides.insert(side);
+	}
+	
+	prevSide = side;
+
+	if(x + delta + 1 > map.at(0).size() || map.at(0).at(x + delta + 1) == ' ')
+	{
+		y = y + delta + 1;
+		side = cubeSideEdges.at(prevSide).at(down);
+		puzzleCube.faceUpSideDirection.at(side) = puzzleCube.faceUpSideDirection.at(prevSide);
+	}
+	else
+	{
+		x = x + delta + 1;
+		side = cubeSideEdges.at(prevSide).at(right);
+		puzzleCube.faceUpSideDirection.at(side) = right;
+		
+	}
+	puzzleCube.cubeFaces.at(side) = getCubeFace(map, x, y, delta);
+	puzzleCube.cubeFaceCoordinateOffset.at(side) = { x,y };
+	checkedSides.insert(side);
+	prevSide = side;
+
+
+
+
+	return puzzleCube;
+}
+*/
+
+long long walkMap(const std::vector<std::string>& map, const std::vector<std::variant<int, char>>& directions)
 {
 	int x = 0, //x increases right
 		y = 0, //y increases down
-		deltaX = 0,
-		deltaY = inMap.size();
+		deltaX = map.at(0).size(),
+		deltaY = map.size();
 
 	Cardinal heading = right;
-
-	// find largest delta x and pad map
-	std::vector<std::string> map;
-	for (std::string line : inMap)
-	{
-		if (line.size() > deltaX) deltaX = line.size();
-	}
-	for (std::string line : inMap)
-	{
-		map.push_back(line + std::string(deltaX - line.size(), ' '));
-	}
-	
 
 	//find initial x position
 	x = map.at(0).find_first_not_of(' ');
@@ -165,24 +376,12 @@ int main()
 	
 	std::cout << "Part 1:\n";
 	//std::cout << "Final password (test) is: " << walkMap(testMap, testDirections) << "\n";
-	std::cout << "Final password (puzzle) is: " << walkMap(map, directions) << "\n";
+	//std::cout << "Final password (puzzle) is: " << walkMap(map, directions) << "\n";
 
-	
-	/*
-	for (std::string line : testMap)
-	{
-		std::cout << line + "\n";
-	}
-	std::cout << "\n";
+	std::cout << testMap.size() << " " << testMap.at(0).size() << "\n";
+	std::cout << std::gcd(testMap.size(), testMap.at(0).size()) << "\n";
+	std::cout << std::gcd(map.size(), map.at(0).size()) << "\n";
 
-	for (auto& move : testDirections)
-	{
-		std::visit([](auto&& arg) {
-			using T = std::decay_t<decltype(arg)>;
-			if constexpr (std::is_same_v<T, int>) std::cout << arg << "\n";
-			else if constexpr (std::is_same_v<T, char>) std::cout << arg << "\n";
-			}, move);
-	}*/
 
 	return 0;
 }
--- day22/day22.h
+++ day22/day22.h
@@ -8,8 +8,34 @@
 
 #include <string>
 #include <vector>
+#include <array>
 #include <tuple>
 #include <variant>
+#include <set>
+#include <queue>
 
 #include <regex>
 #include <type_traits>
+#include <numeric>
+
+enum Cardinal { right, down, left, up, CARDINAL_SIZE };
+enum Side { Top, Bottom, Front, Back, Left, Right };
+
+//pre-compiled vector of all cube side connections
+static const std::vector<std::vector<Side>> cubeSideEdges = 
+{
+	{Right,Front,Left,Back}, {Left,Back,Right,Front}, //Top, Bottom
+	{Right,Bottom,Left,Top}, {Left,Bottom,Right,Top}, //Front, Back
+	{Front,Bottom,Back,Top}, {Back,Bottom,Left,Top}   //Left, Right
+};
+
+struct Cube
+{
+	std::vector<std::string> map; //map of cube net
+	std::array<std::tuple<std::vector<std::string>>, 6> cubeFaces; //sub-maps of cube faces (index = Side)
+	std::array<Cardinal,6> faceUpSideDirection; //which global direction the top of the sub-map is pointing to (index = Side)
+	std::array<std::array<Cardinal, 4>, 6> edgeDirectionChanges; // new heading on face transfer (outer array index = Side, inner array index = Cardinal)
+	std::array<std::tuple<int, int>, 6> cubeFaceCoordinateOffset; //upper left coordinate of cube face on map (index = Side)
+
+
+};
