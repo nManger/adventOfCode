185592553efb93681149c4c9bde0c7f48189a2c1
Natascha Manger <rommy3132@gmail.com>
2023-01-06 20:36:35 +0100

    Solution day 16, part 2

--- day16/day16.cpp
+++ day16/day16.cpp
@@ -69,50 +69,203 @@ std::tuple<network,std::vector<int>> preparePuzzleInput(std::string filename)
 	return { caveNetwork,flowRates };
 }
 
-
-int walkToNextCave(const CaveExploration& explorerKit,int location, int timer, int pressure, std::vector<int> path)
+std::vector<std::vector<int>>permutations(int k, std::vector<int> &A)
 {
-	int prs, prsMax = 0;
+	if (k == 1) return { std::vector(A) };
+	
+	std::vector<std::vector<int>> res;
+	std::vector<std::vector<int>> subRes;
+
+	subRes = permutations(k-1,A);
+	res.insert(res.end(), subRes.begin(), subRes.end());
+	
+	for (int i = 0; i < k - 1; ++i)
+	{
+		int dummy;
+		//swap A_i <-> A_(k-1)
+		if (k % 2 == 0)
+		{
+			dummy = A[i];
+			A[i] = A[k - 1];
+			A[k - 1] = dummy;
+		}
+		//swap A_0 <-> A_(k-1)
+		else
+		{
+			dummy = A[0];
+			A[0] = A[k - 1];
+			A[k - 1] = dummy;
+		}
+		subRes = permutations(k - 1, A);
+		res.insert(res.end(), subRes.begin(), subRes.end());
+	}
+	return res;
+}
+
+///recursive walk to next cave
+///parameters: 
+///		explorerKit = cave exploration object containing network information
+///		remainingNodes = vector of unexplored nodeIDs
+///		node = vector ofcurrent nodeIDs of all players 
+///		timer = remaining time
+///		pressure = sum of all pressure released by previously opened valves over their respective total open time
+///		maxPressure = maximum pressure value found before
+///		playerRemDistance = vector of remaining distances of all players incl. valve opening time, players with entry 0 will move during this step
+int walkToNextCave(const CaveExploration& explorerKit, std::vector<int> remainingNodes, std::vector<int> node, int timer, int pressure = 0, int maxPressure=0, std::vector<int> playerRemDistance = {0})
+{
 	//continue walking if we have time
 	if (timer >0) {
 
-		//get all nodes not yet visited
-		std::vector<std::tuple<int, int>> remNodes;
-		bool found = false;
-		for (auto& branch : explorerKit.caveNetwork.at(location))
+		int movingPlayers = 0;
+		//find distances to remaining nodes for players with dist=0 and update pressure
+		std::vector<std::map<int,int>> distances;//map of nodeID in remainingNodes to corresponding distance for each moving player
+		for (int i = 0; i < node.size(); ++i)
 		{
-			for (int loc : path)
+			if (playerRemDistance.at(i) == 0)
 			{
-				if (loc == get<0>(branch))
+				distances.push_back(std::map<int,int>());
+				
+				//if we are not in start node: players with dist=0 open their valve in this step
+				if (node[0] != explorerKit.flowRates.size() - 1)
 				{
-					found = true;
-					break;
+					pressure += timer * explorerKit.flowRates.at(node[i]);
+				}
+				for (auto jt = remainingNodes.begin(); jt != remainingNodes.end(); ++jt)
+				{
+					for (auto [nodeID, dist] : explorerKit.caveNetwork.at(node[i]))
+					{
+						if (nodeID == *jt) distances.at(movingPlayers).insert({ nodeID,dist });
+					}
+				}
+				++movingPlayers;
+			}
+		}
+
+		//if we have no more nodes to walk into
+		if (remainingNodes.empty())
+		{
+			//skip ahead and finish all other players walks
+			if (node.size() > 1)
+			{
+				for (int i = 0; i < node.size(); ++i)
+				{
+					if (playerRemDistance[i] > 0)
+					{
+						pressure += (timer-playerRemDistance[i])* explorerKit.flowRates.at(node[i]);
+					}
 				}
 			}
-			if (!found) remNodes.push_back(branch);
-			found = false;
+			return pressure;
 		}
-		//we've visited all nodes
-		if (remNodes.empty())
+
+		//otherwise: check all possible remaining nodes and return maximum relased pressure of all alternatives
+
+		//build an estimator function based on the remaining distances
+		//assume you could open all remaining valves this instance: if the total pressure is still smaller than maxPressure, we can never reach maxPressure in this branch, so stop trying
+		int testPrs = pressure;
+		for (int id : remainingNodes)
 		{
-			return pressure + (timer-1) * explorerKit.flowRates.at(location);
+			testPrs += (explorerKit.flowRates[id] * (timer-1));
 		}
-		path.push_back(location);
-		if(location != explorerKit.flowRates.size()-1) timer -= 1; //open valve
-		for (auto& branch : remNodes)
+		if (node.size() > 1)
 		{
-			prs = pressure + timer * explorerKit.flowRates.at(location);
-			prs = walkToNextCave(explorerKit, get<0>(branch), timer - get<1>(branch),prs,path);
-			if (prs > prsMax) prsMax = prs;
+			for (int i = 0; i < node.size(); ++i)
+			{
+				if (playerRemDistance[i] > 0)
+				{
+					testPrs += (timer - playerRemDistance[i]) * explorerKit.flowRates.at(node[i]);
+				}
+			}
 		}
+		if (testPrs < maxPressure) return 0;
+
+		//get all combinations of remaining nodes to walk to for all moving players (max 2 players)
+		std::vector<std::vector<int>> permList;
+		if (movingPlayers == 1)
+		{
+			for (int id : remainingNodes)
+			{
+				permList.push_back({ id });
+			}
+		}
+		else
+		{
+			for (int i = 0; i < remainingNodes.size(); ++i)
+			{
+				for (int j = 0; j < remainingNodes.size(); ++j)
+				{
+					if (i == j) continue;
+					//if we are in the start node, we only need all combinations, not all permutations
+					if (node[0] == explorerKit.flowRates.size() - 1 && i > j) continue;
+					permList.push_back({ remainingNodes.at(i),remainingNodes.at(j) });
+				}
+			}
+		}
+
+		//go through all found permutations
+		while(!permList.empty())
+		{
+			//get current permutation
+			std::vector perm = permList.back();
+			permList.pop_back();
+
+			//get new remaining nodes without all nodes to be walked to
+			std::vector<int> remNodes;
+			for (int otherNode : remainingNodes)
+			{
+				bool nodeFound = false;
+				for (int newNode : perm)
+				{
+					if (otherNode == newNode)
+					{
+						nodeFound = true;
+						break;
+					}
+				}
+				if (!nodeFound) remNodes.push_back(otherNode);
+			}
+
+			//get new distances for players, find min distance and set new destination nodes for players with dist=0
+			int n = 0, steps=std::numeric_limits<int>::max(), prs;
+			std::vector<int> playerDist;
+
+			for (int i = 0; i < playerRemDistance.size(); ++i)
+			{
+				int dist = playerRemDistance[i];
+				if (dist == 0)
+				{
+					node.at(i) = perm[n];
+					dist = distances.at(n).at(node.at(i))+1; //walking distance + valve opening
+					++n;
+				}
+				playerDist.push_back(dist);
+				if ( dist < steps) steps = dist;
+			}
+
+			//decrease distances for next step
+			for (int i = 0; i < playerDist.size(); ++i)
+			{
+				playerDist[i] -= steps;
+			}
+
+			prs = walkToNextCave(explorerKit, remNodes, node, timer-steps, pressure, maxPressure, playerDist);
+			/*std::cout << std::format("{:5d} {:5d} {:5d} {:5d} ",prs,pressure,maxPressure,timer);
+			for (int id : node)
+			{
+				std::cout << id << " ";
+			}
+			
+			std::cout << "\n";*/
+			if (prs > maxPressure) maxPressure = prs;
+		}
+		return maxPressure;
 	}
-	//we ran out of time, return 
+	//we ran out of time, return input pressure
 	else
 	{
 		return pressure;
 	}
 	
-	return prsMax;
 }
 
 std::tuple< network, std::vector<int>>collapseNetwork(const network& caveNetwork, const std::vector<int>& flowRates)
@@ -121,7 +274,7 @@ std::tuple< network, std::vector<int>>collapseNetwork(const network& caveNetwork
 	std::vector<int> nonZeroFlowNodes;
 	network collapsedCaveNetwork;
 
-	int networkSize = caveNetwork.size();
+	size_t networkSize = caveNetwork.size();
 	//initialize distance matrix to max_short ~ inf
 	for (int i = 0; i < networkSize;i++)
 	{
@@ -156,7 +309,7 @@ std::tuple< network, std::vector<int>>collapseNetwork(const network& caveNetwork
 	}
 
 	//get mapping of old to new node number and flow rates for reduced network
-	int i = 0; //new node number
+	//int i = 0; //new node number
 	std::vector<int> oldNewNetworkMap;
 	for (int n = 0; n<flowRates.size();n++)
 	{
@@ -164,7 +317,7 @@ std::tuple< network, std::vector<int>>collapseNetwork(const network& caveNetwork
 		{
 			oldNewNetworkMap.push_back(n);
 			nonZeroFlowNodes.push_back(flowRates.at(n));
-			i++;
+			//i++;
 		}
 	}
 
@@ -191,25 +344,40 @@ std::tuple< network, std::vector<int>>collapseNetwork(const network& caveNetwork
 	collapsedCaveNetwork.push_back(neighbours);
 	nonZeroFlowNodes.push_back(0);
 
+	//print new network
+	/*for (int i = 0; i < oldNewNetworkMap.size(); ++i)
+	{
+		std::cout << i << " " << oldNewNetworkMap.at(i) << "\n";
+	}
+	std::cout << "\n";*/
+
 	return { collapsedCaveNetwork,nonZeroFlowNodes };
 }
 
-int exploreCaves(const network& caveNetwork, const std::vector<int>& flowRates)
+int exploreCaves(const network& caveNetwork, const std::vector<int>& flowRates, int players=1, int timer=30)
 {
+	if (players > 2)
+	{
+		std::cout << "Cannot accomodate more than 2 players at this time.\n";
+		exit(1);
+	}
+
 	auto [collapsedCaveNetwork,collFlowRates] = collapseNetwork(caveNetwork,flowRates);
 
-
 	CaveExploration explorerKit = { collapsedCaveNetwork,collFlowRates};
+	std::vector<int> remNodes = std::vector<int>(collFlowRates.size()-1);
+	std::iota(remNodes.begin(), remNodes.end(), 0);
 
 	//starting node is last node in network
-	return walkToNextCave(explorerKit, collapsedCaveNetwork.size()-1, 30, 0, std::vector<int>());
+	return walkToNextCave(explorerKit, remNodes, std::vector<int>(players,collapsedCaveNetwork.size()-1), timer, 0, 0, std::vector<int>(players,0));
 }
 
 int main()
 {
 	auto [testCaves, testFlowRates] = preparePuzzleInput("puzzleInput.txt");
 
-	std::cout << exploreCaves(testCaves, testFlowRates);
+	std::cout << exploreCaves(testCaves, testFlowRates,2,26);
+	
 
 	return 0;
 }
--- day16/day16.h
+++ day16/day16.h
@@ -9,12 +9,19 @@
 #include <vector>
 #include <tuple>
 #include <map>
+#include <set>
+#include <queue>
 
 #include <regex>
 #include <limits>
+#include <numeric>
+#include <format>
 
 struct CaveExploration
 {
-	std::vector<std::vector<std::tuple<int,int>>> caveNetwork;
+	//adjacency list for the cave network: store a list of all neighbours for each cave, represent each neighbour as tuple (nodeID,distance) where nodeID == cave index in outer vector
+	std::vector<std::vector<std::tuple<int,int>>> caveNetwork; 
+
+	//list of flow rates for each cave using nodeID as list index
 	std::vector<int> flowRates;
 };
